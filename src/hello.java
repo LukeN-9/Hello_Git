4975c40b651aff758e55c6ea7f4b7319a1f7bf4f
fcyza <fcyza@Fishers_Laptop.unl.edu>
2024-09-03 12:07:33 -0500

    Added partners Name and Major, I filled in because Adam had to leave.

--- src/hello.java
+++ src/hello.java
@@ -4,6 +4,9 @@ public class hello {
 	public static void main(String[] args) {
 		System.out.println("Luke Nash");
 		System.out.println("Computer Science\n");
+		
+		System.out.println("Fisher Cyza");
+		System.out.println("Computer Engineering\n");
 	}
 
 }
